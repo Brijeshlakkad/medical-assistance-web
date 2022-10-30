9cc18d27808ce4dd18ba01c06ae165c5ab73f5e8
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2022-10-30 17:09:14 -0400

    [FIX] header will show the login for patient

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,18 +7,15 @@ import './header.css'
 export default function HeaderComponent({
     userRole
 }) {
-    console.log(userRole);
     return (
         < div className="header" >
             <Link to={PathConstants.Home} className="logo">Lifeline</Link>
             <div className="header-right">
                 {/* <a className="active" href="#home">Home</a> */}
+                <Link to={PathConstants.PatientLogin}>Log In</Link>
                 {userRole === UserRole.PATIENT && <Link to={PathConstants.AssessmentPage}>Take Assesement</Link>}
                 {userRole === UserRole.PATIENT && <a href="#about">Status</a>}
-                {userRole === UserRole.PATIENT && <Link to={PathConstants.PatientLogin}>Log In</Link>}
-
                 {userRole === UserRole.COUNSELOR && <Link to={PathConstants.CounselorLOP}>List of Patients</Link>}
-                {userRole === UserRole.COUNSELOR && <Link to={PathConstants.CounselorSignup}>Sign Up</Link>}
             </div>
         </ div >
     )
