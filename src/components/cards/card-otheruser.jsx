35e1ad0e5ba8681f3b4f2fa70bbc1f5404b79c83
patelrutwik1999 <rutwikpatel290499@gmail.com>
2022-10-31 03:03:28 -0400

    Tried to have five images for signup.

--- src/components/cards/card-otheruser.jsx
+++ src/components/cards/card-otheruser.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './card-otheruser.css'
 import { Link } from 'react-router-dom'
+import LoginImage0 from './images/patient-login.jpg'
 import LoginImage1 from './images/counselor-login.jpg'
 import LoginImage2 from './images/doctors-login.jpg'
 import { PathConstants } from '../../lib/path-constants'
@@ -8,6 +9,14 @@ import { PathConstants } from '../../lib/path-constants'
 export default function CardOtherLoginComponent() {
     return (
         <div className='columns'>
+            <div className='row image-container-login' style={{ paddingRight: "20px" }}>
+                <img src={LoginImage0} className="doctor-image" alt='Mental Health' />
+                <div className='middle'>
+                    <div className='login-text'>
+                        <Link to={PathConstants.PatientLogin} className='login-text'>Are you a Patient?</Link>
+                    </div>
+                </div>
+            </div>
             <div className='row image-container-login' style={{ paddingRight: "20px" }}>
                 <img src={LoginImage1} className="doctor-image" alt='Mental Health' />
                 <div className='middle'>
