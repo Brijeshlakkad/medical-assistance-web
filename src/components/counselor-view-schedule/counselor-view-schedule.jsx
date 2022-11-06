c05221495f2286329db28c10695a9a5d3974a542
patelrutwik1999 <rutwikpatel290499@gmail.com>
2022-11-06 04:01:57 -0500

    Change in CSS (kept it local) and doctor view schedule completed with search implementation

--- src/components/counselor-view-schedule/counselor-view-schedule.jsx
+++ src/components/counselor-view-schedule/counselor-view-schedule.jsx
@@ -1,62 +1,97 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './view-schedule.css'
+import { PathConstants } from '../../lib/path-constants'
+
+function searchFunctionCounselor() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myScheduleTable");
+
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
 
 export default function CounselorViewScheduleComponent() {
     return (
         <>
-            <h2 className='table-header'>Schedule</h2>
+            <h2 className='schedule-table-header' style={{ fontSize: '40px' }}>Appointment Schedule</h2>
             <div>
-                <table className='schedule'>
-                    <tr>
-                        <th>Patient Name</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                    </tr>
-                    <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                    </tr>
-                    <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                    </tr>
-                    <tr>
-                        <td>Ernst Handel</td>
-                        <td>Roland Mendel</td>
-                        <td>Austria</td>
-                    </tr>
-                    <tr>
-                        <td>Island Trading</td>
-                        <td>Helen Bennett</td>
-                        <td>UK</td>
-                    </tr>
-                    <tr>
-                        <td>Laughing Bacchus Winecellars</td>
-                        <td>Yoshi Tannamuri</td>
-                        <td>Canada</td>
-                    </tr>
-                    <tr>
-                        <td>Magazzini Alimentari Riuniti</td>
-                        <td>Giovanni Rovelli</td>
-                        <td>Italy</td>
-                    </tr>
+                <input type="search" id="mySearch" onKeyUp={() => searchFunctionCounselor()}
+                    placeholder="🔍 Search for Date.." title="Type in a name" >
+                </input>
 
+                <table className='schedule' id='myScheduleTable'>
+                    <tbody>
+                        <tr>
+                            <th>Patient Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>View Assessment</th>
+                        </tr>
+                        <tr>
+                            <td>Alfreds Futterkiste</td>
+                            <td>23/4/2002</td>
+                            <td>Germany</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
+                        </tr>
+                        <tr>
+                            <td>Centro comercial Moctezuma</td>
+                            <td>23/4/2004</td>
+                            <td>Mexico</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
+                        </tr>
+                        <tr>
+                            <td>Ernst Handel</td>
+                            <td>23/4/2001</td>
+                            <td>Austria</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
+                        </tr>
+                        <tr>
+                            <td>Island Trading</td>
+                            <td>Helen Bennett</td>
+                            <td>UK</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
+                        </tr>
+                        <tr>
+                            <td>Laughing Bacchus Winecellars</td>
+                            <td>Yoshi Tannamuri</td>
+                            <td>Canada</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
+                        </tr>
+                        <tr>
+                            <td>Magazzini Alimentari Riuniti</td>
+                            <td>Giovanni Rovelli</td>
+                            <td>Italy</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
             <br></br>
-            <div class="pagination table-header">
-                <a href="#">&laquo;</a>
-                <a href="#">1</a>
-                <a href="#" class="active">2</a>
-                <a href="#">3</a>
-                <a href="#">4</a>
-                <a href="#">5</a>
-                <a href="#">6</a>
-                <a href="#">&raquo;</a>
+            <div class="schedule-pagination schedule-table-header">
+                <Link to={PathConstants.CounselorHome} >&laquo;</Link>
+                <Link to={PathConstants.CounselorHome} >1</Link>
+                <Link to={PathConstants.CounselorHome} className="active">2</Link>
+                <Link to={PathConstants.CounselorHome} >3</Link>
+                <Link to={PathConstants.CounselorHome} >4</Link>
+                <Link to={PathConstants.CounselorHome} >5</Link>
+                <Link to={PathConstants.CounselorHome} >6</Link>
+                <Link to={PathConstants.CounselorHome} >&raquo;</Link>
             </div>
-
+            <div className='extra'>
+            </div>
         </>
     )
 }
--- src/components/doctor-view-schedule/doctor-view-schedule.jsx
+++ src/components/doctor-view-schedule/doctor-view-schedule.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../counselor-view-schedule/view-schedule.css'
+import { PathConstants } from '../../lib/path-constants'
+import { Link } from 'react-router-dom';
 
-function myFunction() {
+function searchFunctionDoctor() {
     var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
+    input = document.getElementById("mySearch");
     filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
+    table = document.getElementById("myScheduleTable");
     console.log(input);
     console.log(table);
     tr = table.getElementsByTagName("tr");
@@ -26,62 +28,72 @@ export default function DoctorViewScheduleComponent() {
 
     return (
         <>
-            <h2 className='table-header'>Schedule</h2>
-            <div>
-                <input type="text" id="myInput" onKeyUp={() => myFunction()} placeholder="Search for Date.." title="Type in a name" />
+            <h2 className='schedule-table-header' style={{ fontSize: '40px' }}>Appointment Schedule</h2>
 
-                <table className='schedule' id='myTable'>
+            <div>
+                <input type="search" id="mySearch" onKeyUp={() => searchFunctionDoctor()}
+                    placeholder="🔍 Search for Date.." title="Type in a name" >
+                </input>
+
+                <table className='schedule' id='myScheduleTable'>
                     <tbody>
                         <tr>
                             <th>Patient Name</th>
                             <th>Date</th>
                             <th>Time</th>
+                            <th>View Assessment</th>
                         </tr>
                         <tr>
                             <td>Alfreds Futterkiste</td>
                             <td>Maria Anders</td>
                             <td>Germany</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
                         </tr>
                         <tr>
                             <td>Centro comercial Moctezuma</td>
                             <td>Francisco Chang</td>
                             <td>Mexico</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
                         </tr>
                         <tr>
                             <td>Ernst Handel</td>
                             <td>Roland Mendel</td>
                             <td>Austria</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
                         </tr>
                         <tr>
                             <td>Island Trading</td>
                             <td>Helen Bennett</td>
                             <td>UK</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
                         </tr>
                         <tr>
                             <td>Laughing Bacchus Winecellars</td>
                             <td>Yoshi Tannamuri</td>
                             <td>Canada</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
                         </tr>
                         <tr>
                             <td>Magazzini Alimentari Riuniti</td>
                             <td>Giovanni Rovelli</td>
                             <td>Italy</td>
+                            <td><Link className='view-file-button' to={PathConstants.CounselorLOP}>View File</Link></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
             <br></br>
-            <div className="pagination table-header">
-                <a href="#">&laquo;</a>
-                <a href="#">1</a>
-                <a href="#" className='active'>2</a>
-                <a href="#">3</a>
-                <a href="#">4</a>
-                <a href="#">5</a>
-                <a href="#">6</a>
-                <a href="#">&raquo;</a>
+            <div className="schedule-pagination schedule-table-header">
+                <Link to={PathConstants.CounselorHome} >&laquo;</Link>
+                <Link to={PathConstants.CounselorHome} >1</Link>
+                <Link to={PathConstants.CounselorHome} className="active">2</Link>
+                <Link to={PathConstants.CounselorHome} >3</Link>
+                <Link to={PathConstants.CounselorHome} >4</Link>
+                <Link to={PathConstants.CounselorHome} >5</Link>
+                <Link to={PathConstants.CounselorHome} >6</Link>
+                <Link to={PathConstants.CounselorHome} >&raquo;</Link>
             </div>
-
+            <div className='extra'></div>
         </>
     )
 }
