a87972342bfd1b2772caa707ebd22cd0b887bb6e
DESKTOP-NPH301G\urvis <urvish.tanti07@gmail.com>
2022-11-12 15:31:13 -0500

    fix: UI updates for list of doctor tab

--- src/components/list-of-doctor/list-of-doctor.jsx
+++ src/components/list-of-doctor/list-of-doctor.jsx
@@ -6,58 +6,74 @@ import { PaginationComponent } from "../pagination/pagination";
 import PatientInfo from "../patient-info/patient-info";
 import "./list-of-doctor.css";
 
-export function ListOfDoctor({ payload, activePatient, onSelect, errorMessage }) {
-    const navigate = useNavigate();
-    const onPageChange = (page) => {
-        navigate({ pathname: PathConstants.CounselorLOP, search: `page=${page}` });
-    }
-    return (
-        <>
-            <div className="list-of-doctor-patient">
-                <PatientInfo patient={activePatient.patient} createdAt={activePatient.createdAt} />
-                <div className="list-of-doctor">
-                    <h1>
-                        Assign Patient to Doctor
-                    </h1>
-                    <table className="assign-pat" id="myTable">
-                        <thead className="tab-head-doc">
-                            <tr>
-                                <th>Doctor's Name</th>
-                                <th>Doctor's Email ID</th>
-                                <th>Active Patients List</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                payload.content.map((doctorRecord, index) => {
-                                    return <tr key={`doctor-${index}`}>
-                                        <td>{doctorRecord.fullName}</td>
-                                        <td>{doctorRecord.emailAddress}</td>
-                                        <td>{doctorRecord.currentPatients}</td>
-                                        <td><button className="button-list-of-doctor-patient" onClick={() => {
-                                            onSelect(doctorRecord.registrationNumber);
-                                        }}>select</button></td>
-                                    </tr>
-                                })
-                            }
-                        </tbody>
-                    </table>
-                    {errorMessage && <ErrorMessage>
-                        {errorMessage}
-                    </ErrorMessage>
-                    }
-                </div>
-            </div>
-            <br></br>
+export function ListOfDoctor({
+  payload,
+  activePatient,
+  onSelect,
+  errorMessage,
+}) {
+  const navigate = useNavigate();
+  const onPageChange = (page) => {
+    navigate({ pathname: PathConstants.CounselorLOP, search: `page=${page}` });
+  };
+  return (
+    <>
+      <h1 className="pat-to-doc">Assign Patient to Doctor</h1>
+      <div className="list-of-doctor-patient">
+        <PatientInfo
+          patient={activePatient.patient}
+          createdAt={activePatient.createdAt}
+        />
+        <div className="list-of-doctor">
+          <table className="assign-pat" id="myTable">
+            <thead className="tab-head-doc">
+              <tr>
+                <th>Doctor's Name</th>
+                <th>Doctor's Email ID</th>
+                <th>Active Patients List</th>
+                <th>Select Doctor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {payload.content.map((doctorRecord, index) => {
+                return (
+                  <tr key={`doctor-${index}`}>
+                    <td>{doctorRecord.fullName}</td>
+                    <td>{doctorRecord.emailAddress}</td>
+                    <td>{doctorRecord.currentPatients}</td>
+                    <td>
+                      <button
+                        className="button-list-of-doctor-patient"
+                        onClick={() => {
+                          onSelect(doctorRecord.registrationNumber);
+                          alert(
+                            "Your appointment with Patient Name has been confirmed"
+                          );
+                        }}
+                      >
+                        select
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="doctor-list-pagination">
             <PaginationComponent
-                onPageChange={onPageChange}
-                pageNumber={payload.pageable.pageNumber}
-                totalPages={payload.totalPages}
-                first={payload.first}
-                last={payload.last}
+              onPageChange={onPageChange}
+              pageNumber={payload.pageable.pageNumber}
+              totalPages={payload.totalPages}
+              first={payload.first}
+              last={payload.last}
             />
-            <div className='extra'></div>
-        </>
-    );
+          </div>
+          {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+        </div>
+      </div>
+      <br></br>
+
+      <div className="extra"></div>
+    </>
+  );
 }
