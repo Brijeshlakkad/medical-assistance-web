ad5a891b7b2a58bff9dd1e5ae9aec973b4143bb8
DESKTOP-NPH301G\urvis <urvish.tanti07@gmail.com>
2022-11-28 15:11:10 -0500

    Report UI fixes

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const LoadingDiv = styled.div`
-    display: flex;
-    justify-content: center;
-`
+  margin-top: 100px;
+  height: 550px;
+  display: flex;
+  justify-content: center;
+`;
 export function LoadingComponent() {
-    return <LoadingDiv>
-        <span>Loading</span>
+  return (
+    <LoadingDiv>
+      <span>Loading</span>
     </LoadingDiv>
+  );
 }
-
--- src/components/manager-data-analytics/dashboard-cards-component.jsx
+++ src/components/manager-data-analytics/dashboard-cards-component.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
-import './dashboard-cards.css'
-import { faUser, faPersonChalkboard, faUserDoctor, faUserNurse } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import React from "react";
+import "./dashboard-cards.css";
+import {
+  faUser,
+  faPersonChalkboard,
+  faUserDoctor,
+  faUserNurse,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function DashboardCardsComponent({ payload }) {
-    return (
-        <div className='dashboard-card-row'>
-            <div className='dashboard-card-columns'>
-                <div className='dashboard-card-cards cards-color-users'>
-                    <FontAwesomeIcon icon={faUser} className='users-cards'></FontAwesomeIcon>
-                    <h3 style={{ fontSize: "1.7rem" }}>Total Users</h3>
-                    <p className='show-data-cards'>{payload.numTotal}</p>
-                </div>
-            </div>
-
-            <div className='dashboard-card-columns'>
-                <div className='dashboard-card-cards cards-color-assessments'>
-                    <FontAwesomeIcon icon={faPersonChalkboard} className='users-cards'></FontAwesomeIcon>
-                    <h3 style={{ fontSize: "1.7rem" }}>Assessments</h3>
-                    <p className='show-data-cards'>{payload.numAttemptedAssessment}</p>
-                </div>
-            </div>
-
-            <div className='dashboard-card-columns'>
-                <div className='dashboard-card-cards cards-color-meeting-counselor'>
-                    <FontAwesomeIcon icon={faUserNurse} className='users-cards'></FontAwesomeIcon>
-                    <h3 style={{ fontSize: "1.7rem" }}>Meeting Councellor</h3>
-                    <p className='show-data-cards'>{payload.numHasCounselorAppointment}</p>
-                </div>
-            </div>
-
-            <div className='dashboard-card-columns'>
-                <div className='dashboard-card-cards cards-color-meeting-doctor'>
-                    <FontAwesomeIcon icon={faUserDoctor} className='users-cards'></FontAwesomeIcon>
-                    <h3 style={{ fontSize: "1.7rem" }}>Meeting Doctor</h3>
-                    <p className='show-data-cards'>{payload.numHasDoctorAppointment}<span>({payload.numInProcessingDoctorAppointment})</span></p>
-                </div>
-            </div>
+  return (
+    <div className="dashboard-card-row">
+      <div className="dashboard-card-columns">
+        <div className="dashboard-card-cards cards-color-users">
+          <FontAwesomeIcon
+            icon={faUser}
+            className="users-cards"
+          ></FontAwesomeIcon>
+          <h3 style={{ fontSize: "1.7rem" }}>Total Users</h3>
+          <p className="show-data-cards">{payload.numTotal}</p>
         </div>
-    )
+      </div>
+
+      <div className="dashboard-card-columns">
+        <div className="dashboard-card-cards cards-color-assessments">
+          <FontAwesomeIcon
+            icon={faPersonChalkboard}
+            className="users-cards"
+          ></FontAwesomeIcon>
+          <h3 style={{ fontSize: "1.7rem" }}>Assessments</h3>
+          <p className="show-data-cards">{payload.numAttemptedAssessment}</p>
+        </div>
+      </div>
+
+      <div className="dashboard-card-columns">
+        <div className="dashboard-card-cards cards-color-meeting-counselor">
+          <FontAwesomeIcon
+            icon={faUserNurse}
+            className="users-cards"
+          ></FontAwesomeIcon>
+          <h3 style={{ fontSize: "1.6rem" }}>Meeting Counselor</h3>
+          <p className="show-data-cards">
+            {payload.numHasCounselorAppointment}
+          </p>
+        </div>
+      </div>
+
+      <div className="dashboard-card-columns">
+        <div className="dashboard-card-cards cards-color-meeting-doctor">
+          <FontAwesomeIcon
+            icon={faUserDoctor}
+            className="users-cards"
+          ></FontAwesomeIcon>
+          <h3 style={{ fontSize: "1.7rem" }}>Meeting Doctor</h3>
+          <p className="show-data-cards">
+            {payload.numHasDoctorAppointment}
+            <span>({payload.numInProcessingDoctorAppointment})</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
