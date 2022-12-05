5e3e6937c57dc825328032c5c8f2c5567798728b
DESKTOP-NPH301G\urvis <urvish.tanti07@gmail.com>
2022-12-04 19:30:25 -0500

    fix container header and footer, placeholder value and label

--- src/containers/counselor-view-schedule.jsx
+++ src/containers/counselor-view-schedule.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
-import FooterComponent from '../components/footer/footer'
-import { LoadingComponent } from '../components/loading/loading'
-import { ViewScheduleComponent } from '../components/view-schedule/view-schedule'
-import { RequestState, UserRole } from '../lib/types'
-import { fetchAppointments } from '../store/actions/counselor-appointments'
-import Header from './header'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import FooterComponent from "../components/footer/footer";
+import { LoadingComponent } from "../components/loading/loading";
+import { ViewScheduleComponent } from "../components/view-schedule/view-schedule";
+import { RequestState, UserRole } from "../lib/types";
+import { fetchAppointments } from "../store/actions/counselor-appointments";
+import Header from "./header";
 
 export default function CounselorViewSchedule() {
-    const dispatch = useDispatch();
-    const location = useLocation();
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-    const requestState = useSelector(state => state.counselorAppointments.state);
+  const requestState = useSelector(
+    (state) => state.counselorAppointments.state
+  );
 
-    useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-        const page = queryParams.get("page");
-        dispatch(fetchAppointments(page));
-    }, [dispatch, location]);
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const page = queryParams.get("page");
+    dispatch(fetchAppointments(page));
+  }, [dispatch, location]);
 
-    const payload = useSelector(state => state.counselorAppointments.payload);
+  const payload = useSelector((state) => state.counselorAppointments.payload);
 
-    return (
-        requestState !== RequestState.COMPLETED ?
-            <LoadingComponent /> : <>
-                <Header></Header>
-                <ViewScheduleComponent
-                    payload={payload}
-                    role={UserRole.COUNSELOR}
-                />
-                <FooterComponent></FooterComponent>
-            </>
-    )
+  return (
+    <>
+      <Header></Header>
+      {requestState !== RequestState.COMPLETED ? (
+        <LoadingComponent />
+      ) : (
+        <ViewScheduleComponent payload={payload} role={UserRole.COUNSELOR} />
+      )}
+      <FooterComponent></FooterComponent>
+    </>
+  );
 }
